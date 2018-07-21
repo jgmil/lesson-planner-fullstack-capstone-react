bbe169a49bcad3930b154a1177bd855b32ff537e
Julia Miller <opie.julia@gmail.com>
2018-07-20 21:00:17 -0400

    fully functional react app with the header component

--- js/components/header.js
+++ js/components/header.js
@@ -11,10 +11,29 @@ configure({ adapter: new Adapter() });
 
 export default function Header(props) {
     return (
-        <header>
-        <h1>Not Just Luck</h1>
-        <a href="#" id="js-signin-link" alt="Sign In">Sign In</a>
-        <a href="#" id="js-signout-link" alt="Sign Out">Sign Out</a>
+        <header className="clearfix" role="banner" id="landing">
+            <a href="#" id="home">
+                <h2>Lesson planner</h2>
+                <h3 className="user-banner">Welcome, Mrs. Miller!</h3>
+            </a>
+
+            <nav role="navigation" className="float-right">
+                <div className="landing-nav">
+                    <a href="#log-in" id="log-in-nav-link">Log in</a>
+                    <a href="#create-account-form" id="create-account-nav-link">Create an account</a>
+                    <a href="#more-info" id="info-link">More info</a>
+                </div>
+                <div className="dashboard-nav">
+                    <button className="create-subject-nav">Add a subject</button>
+                    <button className="create-unit-nav">Add a unit</button>
+                    <button className="create-lesson-nav">Add a lesson</button>
+                    <button id="log-out-link">Log out</button><br />
+                </div>
+                <div className="subject-shortcuts float-right">
+                </div>
+            </nav>
+
+            <input type="hidden" id="loggedInUser" value="" />
         </header>
     )
 }
